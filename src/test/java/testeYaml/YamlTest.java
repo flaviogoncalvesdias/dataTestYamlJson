aeac660971c14b0779c19574d298ae6a102670e2
Flávio Dias <flavio.goncalvesdias@gmail.comm>
2020-09-30 20:44:13 -0300

    YAML POJO

--- src/test/java/testeYaml/YamlTest.java
+++ src/test/java/testeYaml/YamlTest.java
@@ -7,14 +7,14 @@ import java.io.File;
 public class YamlTest {
 
     public static void main(String[] args) {
-        Url url = new Url();
+        DadosTesteYaml url = new DadosTesteYaml();
         ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
         try {
-            url = objectMapper.readValue(new File("src/test/java/test.yaml"), Url.class);
+            url = objectMapper.readValue(new File("./resources/test.yaml"), DadosTesteYaml.class);
         }catch (Exception ex){
             ex.printStackTrace();
         }
 
-        System.out.println("Url buscada: "+url.getBaseUrl());
+        System.out.println("Url buscada: "+url.getUrl());
     }
 }
